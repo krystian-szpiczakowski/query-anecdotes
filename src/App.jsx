122b9c7c5f1895d26cf7925145208b21fda1bd24
Krystian Szpiczakowski <szpiczakowski.krystian@gmail.com>
2024-05-31 15:14:55 +0200

    Part 6.20 - Retrieve anecdotes with React Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { useQuery } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAnecdotes } from './requests'
 
 const App = () => {
 
@@ -7,13 +9,21 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: 1
+  })
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
---
+++ src/requests.js
@@ -0,0 +1,7 @@
+import axios from "axios"
+
+export const getAnecdotes = () => {
+    return axios
+    .get("http://localhost:3001/anecdotes")
+    .then(response => response.data)
+}
