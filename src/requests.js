aa0831338687abf4ced1169387bff5c45f618b3b
Krystian Szpiczakowski <szpiczakowski.krystian@gmail.com>
2024-05-31 15:32:39 +0200

    Part 6.21 - Send anecdote to server

--- src/requests.js
+++ src/requests.js
@@ -1,7 +1,12 @@
 import axios from "axios"
 
+const baseUrl = "http://localhost:3001/anecdotes"
+
 export const getAnecdotes = () => {
     return axios
-    .get("http://localhost:3001/anecdotes")
-    .then(response => response.data)
+        .get(baseUrl)
+        .then(response => response.data)
 }
+
+export const createAnecdote = newNote =>
+    axios.post(baseUrl, newNote).then(res => res.data)
